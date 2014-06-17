ee27a5f1c37f7af3f5b9a941f9658afdc15b5136
Luigi Esposito <jok3rplay@gmail.com>
2014-06-17 18:33:33 +0200

    -Completata la sezione Segnalazioni e Reclami. -Aggiunta al progetto la libreria commons-net-1.4.jar di Apache per caricare via FTP sul server remoto le foto allegate alle segnalazioni.

--- ComuneDiScafati/src/com/comune/scafati/Activity_SegnalazioneReclami.java
+++ ComuneDiScafati/src/com/comune/scafati/Activity_SegnalazioneReclami.java
@@ -1,38 +1,96 @@
 package com.comune.scafati;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+
 import android.location.LocationManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.DialogInterface.OnCancelListener;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 import android.location.Location;
 import android.location.LocationListener;
 import android.app.AlertDialog;
 import android.widget.TextView;
 
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.comune.scafati.Activity_NewsEdEventi.DummySectionFragment2.task;
+import com.comune.scafati.Activity_NewsEdEventi.DummySectionFragment2.taskAll;
+
 /* Questa classe viene visualizzata quando l'utente seleziona
  * il pulsante "Segnalazioni e Reclami" nel MainActivity. */
 public class Activity_SegnalazioneReclami extends Activity {
 
-	private static final int SELECT_PICTURE = 1;
+
+	private static final int REQUEST_CODE = 0;
 	TextView posText;
 	Button posButton;
-	boolean undo;
+	boolean undo = false;
 	GPSLocationListener gpsListener;
-	
+	boolean OpResult;
+	String ogg,descr,loc,imgurl;
+	private static Bitmap scaledphoto=null;
+    private String filePath=null;
+    private Uri u=null;
+    private Boolean picTaken=false;
+    LocationManager lm;
+    ImageView photo_captured=null;
+    boolean status = false;
+    ProgressDialog pd;
+    boolean localization = false;
+    
 	// Metodo che viene chiamato alla creazione dell'activity.
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_segnrec);
-
+		
+    	posButton = (Button) findViewById(R.id.buttonPos);
+    	photo_captured = (ImageView)findViewById(R.id.imgSegn);
 		// Recupero il bottone dal layout e setto il listener da invocare quando viene cliccato.
 		Button ButtonInvia = (Button) findViewById(R.id.buttonInvia);
 		ButtonInvia.setOnClickListener(new View.OnClickListener() {
@@ -42,12 +100,20 @@ public class Activity_SegnalazioneReclami extends Activity {
          	 // Controllo che tutti i campi siano stati compilati.
          	 if(oggetto.getText().length() != 0 && descrizione.getText().length() != 0)
          	 {
-         		finish();
-         		Context context = getApplicationContext();
-      	    	CharSequence text = "Segnalazione Inviata";
-      	    	int duration = Toast.LENGTH_SHORT;
-      	    	Toast toast = Toast.makeText(context, text, duration);
-      	    	toast.show();
+             	if(undo)
+            	{
+            		lm.removeUpdates(gpsListener);
+            		undo = false;
+            		localization=false;
+            	}
+         		 ogg = oggetto.getText().toString();
+         		 descr = descrizione.getText().toString();
+         		if(picTaken)
+         		{
+         			upLoadPicture();
+         		}
+         		new task().execute();
+         		
          	 }
          	 else
          	 {
@@ -76,30 +142,130 @@ public class Activity_SegnalazioneReclami extends Activity {
     	
     	oggetto.setText(null);
     	descrizione.setText(null);
+    	if(picTaken)
+    	{
+    		photo_captured.setVisibility(View.GONE);
+    		picTaken=false;
+    	}
+    	if(!picTaken)
+    	{
+    		status=true;
+    	}
+    	if(undo || posButton.getText()=="Rimuovi Posizione")
+    	{
+    		lm.removeUpdates(gpsListener);
+    		posText.setTextColor(Color.parseColor("#a7d5e1"));
+    		posText.setText("Caricamento Coordinate GPS in corso...");
+    		posText.setVisibility(View.GONE);
+    		posButton.setText("Rileva Posizione");
+    		undo = false;
+    		localization=false;
+    	}
 	      }
 	
 	public void AllFoto(View view) {  
-		Intent pickIntent = new Intent();
-		pickIntent.setType("image/*");
-		pickIntent.setAction(Intent.ACTION_GET_CONTENT);
-
-		Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-		String pickTitle = "Seleziona o scatta una foto"; // Or get from strings.xml
-		Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
-		chooserIntent.putExtra
-		(
-		  Intent.EXTRA_INITIAL_INTENTS, 
-		  new Intent[] { takePhotoIntent }
-		);
-
-		startActivityForResult(chooserIntent, SELECT_PICTURE);
+		String pictureName=String.format("%d",new Date().getTime());
+		imgurl="http://semplicementech.t15.org/comunescafati/uploads/"+pictureName+".jpg";
+		Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(),  pictureName+".jpg");//save picture (.jpg) on SD Card
+        u=Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,u);      
+        filePath = photo.getAbsolutePath();        
+        startActivityForResult(intent, REQUEST_CODE);
      }
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		super.onActivityResult(requestCode, resultCode, data);
+		 
+		 try {
+
+		             getContentResolver().notifyChange(u, null);  
+		             ContentResolver cr = getContentResolver();     
+		             Bitmap bm = android.provider.MediaStore.Images.Media.getBitmap(cr, u);
+		//ImageView to set the picture taken from camera.
+		             photo_captured.setImageBitmap(bm); 
+		             photo_captured.setVisibility(View.VISIBLE);
+		             picTaken=true; //to ensure picture is taken 
+
+		      }catch(Exception e) {
+		           
+		       e.printStackTrace();   
+		     
+		     }                            
+		}
+	
+	//Method for uploading picture on FtpServer
+	public void upLoadPicture() {
+	        
+	            pd = ProgressDialog.show(Activity_SegnalazioneReclami.this, "Attendere prego...", "Caricamento immagine...", true);
+	            new Thread(){    
+	                       
+	                  @Override        
+	                  public void run() {              
+	                 
+	                        File file = new File(filePath);  
+	                              try {
+	                                   
+	                                    FTPClient client = new FTPClient();
+	                                    client.connect("ftp.semplicementech.t15.org",21);
+
+	                                    client.login("u735935028", "comunescafati"); //this is the login credentials of your ftpserver. Ensure to use valid username and password otherwise it throws exception
+	                                    client.changeWorkingDirectory("/public_html/comunescafati/uploads");
+	                                    Log.e("serverReply: ",client.getReplyString());
+	                                    if (client.getReplyString().contains("250")) {
+	                                        client.setFileType(org.apache.commons.net.ftp.FTP.IMAGE_FILE_TYPE);
+	                                        BufferedInputStream buffIn = null;
+	                                        buffIn = new BufferedInputStream(new FileInputStream(file));
+	                                        client.enterLocalPassiveMode();
+
+	                                        status = client.storeFile(file.getName(),buffIn);
+		                                    Log.e("Status", String.valueOf(status));  
+
+	                                        buffIn.close();
+	                                        client.logout();
+	                                        client.disconnect();
+	                                    }                   
+	                                    file.delete();
+	                              }                                              
+	                              catch (Exception e) {
+	                              }     
+	  	                        pd.dismiss(); 
+	  	             			runOnUiThread(new Runnable() 
+	  	             	        {                
+	  	             	            @Override
+	  	             	            public void run() 
+	  	             	            {
+	                                    Log.e("OpResult", String.valueOf(OpResult));  
+
+	  	             	            	if(OpResult && status)
+	  	             	            	{
+	  	             	            		finish();
+	  	             	            		Context context = getApplicationContext();
+	  	             	            		CharSequence text = "Segnalazione Inviata";
+	  	             	            		int duration = Toast.LENGTH_SHORT;
+	  	             	            		Toast toast = Toast.makeText(context, text, duration);
+	  	             	            		toast.show();
+	  	             	            	}
+	  	             	            	else if(!OpResult || !status)
+	  	             	            	{
+	  	             	            		Context context = getApplicationContext();
+	  	             	            		CharSequence text = "Errore, controlla la tua connessione e riprova.";
+	  	             	            		int duration = Toast.LENGTH_SHORT;
+	  	             	            		Toast toast = Toast.makeText(context, text, duration);
+	  	             	            		toast.show();
+	  	             	            	}
+	  	             	            }
+	  	             	        });
+	  	             		}
+	                  }.start();       
+	}
 	
 	private class GPSLocationListener implements LocationListener {
 		@Override
 		public void onLocationChanged(Location location) {
-
+			localization=true;
+			loc=String.valueOf(location.getLatitude())+String.valueOf(location.getLongitude());
         	posText.setText("Latitudine: "+String.valueOf(location.getLatitude())+"nLongitudine: "+String.valueOf(location.getLongitude()));
         	posButton.setText("Rimuovi Posizione");
 		}
@@ -117,11 +283,9 @@ public class Activity_SegnalazioneReclami extends Activity {
 	
 	public void AllPos(View view) {  
 		
-		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		posText = (TextView) findViewById(R.id.textPos);
     	posText.setVisibility(View.VISIBLE);
-
-    	posButton = (Button) findViewById(R.id.buttonPos);
     	
 		boolean gps = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
 		if(!undo)
@@ -156,6 +320,93 @@ public class Activity_SegnalazioneReclami extends Activity {
 			posText.setVisibility(View.GONE);
 			posButton.setText("Rileva Posizione");
 			undo = false;
+			localization=false;
 		}
 	 }
+	
+	class task extends AsyncTask<String, String, Void>
+	{
+	private ProgressDialog progressDialog = new ProgressDialog(Activity_SegnalazioneReclami.this);
+	    InputStream is = null ;
+	    String result = "";
+	    protected void onPreExecute() {
+	    	OpResult = true;
+	    	   if(!picTaken)
+	    	   {
+	       progressDialog.setMessage("Caricamento in corso...");
+	       progressDialog.show();
+	       progressDialog.setOnCancelListener(new OnCancelListener() {
+		@Override
+			public void onCancel(DialogInterface arg0) {
+			task.this.cancel(true);
+		   }
+		});}
+	     }
+	       @Override
+		protected Void doInBackground(String... params) {
+		  String url_select = "http://semplicementech.t15.org/comunescafati/queryscript/setSegnalazioni.php";
+		    try {
+		  HttpClient httpClient = new DefaultHttpClient();
+		  HttpPost httpPost = new HttpPost(url_select);
+
+	          ArrayList<NameValuePair> param = new ArrayList<NameValuePair>();
+				param.add(new BasicNameValuePair("Oggetto",ogg));
+				param.add(new BasicNameValuePair("Descrizione",descr));
+				if(picTaken)
+				{
+					param.add(new BasicNameValuePair("Immagine",imgurl));
+				}
+				else
+				{
+					param.add(new BasicNameValuePair("Immagine","null"));
+				}
+				if(localization)
+				{
+					param.add(new BasicNameValuePair("Posizione",loc));
+
+				}
+				else
+				{
+					param.add(new BasicNameValuePair("Posizione","null"));
+
+				}
+
+			httpPost.setEntity(new UrlEncodedFormEntity(param));
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+	        is = entity.getContent();
+	        Log.e("pass 1", "connection success ");
+			} catch (Exception e) {
+				OpResult = false;
+			Log.e("log_tag", "Error in http connection "+e.toString());
+			}
+			return null;
+	       }
+	       
+	       protected void onPostExecute(Void v) {
+	    	   if(!picTaken){
+				this.progressDialog.dismiss();
+				if(OpResult)
+         		{
+         		finish();
+         		Context context = getApplicationContext();
+      	    	CharSequence text = "Segnalazione Inviata";
+      	    	int duration = Toast.LENGTH_SHORT;
+      	    	Toast toast = Toast.makeText(context, text, duration);
+      	    	toast.show();
+         		}
+         		else if(!OpResult)
+         		{
+         			Context context = getApplicationContext();
+          	    	CharSequence text = "Errore, controlla la tua connessione e riprova.";
+          	    	int duration = Toast.LENGTH_SHORT;
+          	    	Toast toast = Toast.makeText(context, text, duration);
+          	    	toast.show();
+         		}
+	    	   }
+				return;
+			}
+
+	}
+	
 }
