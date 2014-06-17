5c546dbc8f6b0452f302adfd8fbbb5660b764630
Gianluca Pannullo <lizard92@libero.it>
2014-06-17 17:26:54 +0200

    Aggiunto i bottoni della mappa,chiamate e preferiti nel visualizzatore infoutili. Migliorie fatta alla grafica sempre nel visualizzatore infoutili

--- ComuneDiScafati/src/com/comune/scafati/Activity_VisInfoUtili.java
+++ ComuneDiScafati/src/com/comune/scafati/Activity_VisInfoUtili.java
@@ -1,31 +1,35 @@
 package com.comune.scafati;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 /* Questa classe viene visualizzata quando l'utente seleziona un elemento
  * della lista presente nell'Activity_InformazioniUtili, e serve per
  * visualizzare le informazioni di quel dato elemento. */
 public class Activity_VisInfoUtili extends Activity {
-	
+	ListView listViewVIS;
+	int numInfo;
 	// Metodo che viene chiamato alla creazione dell'activity.
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_activity__visualizzatore_info);
 		Intent intent=getIntent();
-		
-		ImageButton ButtonChiamata;
-		ImageButton ButtonMappa;
-		ImageButton ButtonPreferiti;
 		
 		String tipo = intent.getStringExtra("Tipo");
 		
@@ -45,20 +49,31 @@ public class Activity_VisInfoUtili extends Activity {
 		db.open();
 		// Recupero dal database le informazioni utili del tipo richiesto.
 		Cursor c = db.getIU(tipo);
-		int numInfo = c.getCount();
+		numInfo = c.getCount();
 		
 		// Formatto e visualizzo i dati prelevati dal database.
-		TextView testo =(TextView)findViewById(R.id.descrInfo)
-				;
-		ButtonMappa = (ImageButton)findViewById(R.id.ButtonMappa);
 		
-		for(int i=0;i<numInfo;i++)
-		{
-			c.moveToPosition(i);
-			testo.append("\n"+c.getString(0)+"\n"+c.getString(1));
-				 
-			testo.append("\n\n-------------------\n");
-		}
+		listViewVIS=(ListView)findViewById(R.id.listVisInfoUtili);
+		
+		ArrayList<HashMap<String, String>> listavisinfoutili = new ArrayList<HashMap<String, String>>();
+		listavisinfoutili=riempiLista(listavisinfoutili,numInfo,c);
+		
+	
+		//Costruzione adapter
+		String[] from = { "Titolo","Descrizione","Chiamata","ID", "Mappa"};
+		
+		int[] to={R.id.titolodescr,R.id.descrInfo,R.id.ButtonChiamata,R.id.ButtonPreferito,R.id.ButtonMappa};
+		
+		SimpleAdapter adapter=new SimpleAdapter(
+				 this,
+				 listavisinfoutili,
+                 R.layout.riga_lista_visinfoutili,
+                 from,
+                 to
+                );
+		
+		listViewVIS.setAdapter(adapter);
+		
 		db.close();
 	}
     
@@ -69,5 +84,25 @@ public class Activity_VisInfoUtili extends Activity {
 				menu);
 		return true;
 	}	
+	
+	private ArrayList<HashMap<String, String>> riempiLista(ArrayList<HashMap<String, String>> listavisinfoutili, int numInfo, Cursor c) {
+		for(int i=0;i<numInfo;i++)
+		{
+		 c.moveToPosition(i);
+		 listavisinfoutili.add(creaMappa(c.getString(c.getColumnIndex("Nome")),c.getString(c.getColumnIndex("Descrizione")),c.getString(c.getColumnIndex("Nome")),i));
+		}
+        return listavisinfoutili;
+    }
+	
+	 private HashMap<String, String> creaMappa(String titolo, String descrizione, String Mappa, int i) {
+		 
+		    HashMap<String, String> map = new HashMap<String, String>();
+		    
+		    map.put("Titolo", titolo);
+	    	map.put("Descrizione", descrizione);
+	    	map.put("Mappa", Mappa);
+	 
+	    	return map;
+	    }   
 	
 }
